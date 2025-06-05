f958d155fe9e4844a28337451f6d18b0f55cd4aa
Archie George <archiegeorge23@gmail.com>
2025-06-05 11:43:51 +0100

    update - current working version

--- src/app/utilities/fplData.tsx
+++ src/app/utilities/fplData.tsx
@@ -74,3 +74,77 @@ export async function filteredPlayers(): Promise<PlayerRawData[]> {
   return allPlayers.filter((p) => p.selected_by_percent > 0.5);
 }
 
+export const pointsCardData = [
+  { title: "Total Points", field: "total_points", source: "filtered" },
+  { title: "Game Week Points", field: "event_points", source: "all" },
+  { title: "Bonus Points", field: "bonus", source: "all" },
+  { title: "Form", field: "form", source: "filtered" },
+  { title: "ICT Index", field: "ict_index", source: "filtered" },
+  { title: "Selected By %", field: "selected_by_percent", source: "filtered" },
+];
+
+export const ICTCardData = [
+  { title: "Goals Scored", field: "goals_scored", source: "all" },
+  { title: "Assists", field: "assists", source: "all" },
+  { title: "Threat Rank", field: "threat_rank", source: "all" },
+  { title: "ICT Index", field: "ict_index", source: "all" },
+  { title: "Influence", field: "influence", source: "all" },
+  { title: "Creativity", field: "creativity", source: "all" },
+  { title: "Threat", field: "threat", source: "all" },
+];
+
+ export const reliabilityCardData = [
+  { title: "Starts", field: "starts", source: "all" },
+  { title: "Sub Appearances", field: "substitutes_in", source: "all" },
+  { title: "Minutes Played", field: "minutes", source: "all" },
+  { title: "Status", field: "status", source: "all" },
+  { title: "Yellow Cards", field: "yellow_cards", source: "all" },
+  { title: "Red Cards", field: "red_cards", source: "all" },
+  { title: "Selected By %", field: "selected_by_percent", source: "all" },
+];
+
+ export const ATCardData = [
+  { title: "Games Started", field: "starts", source: "all" },
+  { title: "Expected Goals", field: "expected_goals", source: "all" },
+  { title: "xG Per 90", field: "expected_goals_per_90", source: "all" },
+  { title: "xA", field: "expected_assists", source: "all" },
+  { title: "xA Per 90", field: "expected_assists_per_90", source: "all" },
+  { title: "xGI", field: "expected_goal_involvements", source: "all" },
+  {
+    title: "xGI Per 90",
+    field: "expected_goal_involvements_per_90",
+    source: "all",
+  },
+];
+
+export const DFCardData = [
+  { title: "Clean Sheets", field: "clean_sheets", source: "all" },
+  {
+    title: "Clean Sheets Per 90",
+    field: "clean_sheets_per_90",
+    source: "all",
+  },
+  { title: "Goals Conceded", field: "goals_conceded", source: "all" },
+  {
+    title: "Goals Conceded Per 90",
+    field: "goals_conceded_per_90",
+    source: "all",
+  },
+  {
+    title: "Expected Goals Conceded",
+    field: "expected_goals_conceded",
+    source: "all",
+  },
+  { title: "Own Goals", field: "own_goals", source: "all" },
+  { title: "Blocks", field: "blocks", source: "all" },
+];
+
+export const GKCardData = [
+  { title: "Saves", field: "saves", source: "all" },
+  { title: "Save %", field: "save_percentage", source: "computed" },
+  { title: "Clean Sheets", field: "clean_sheets", source: "all" },
+  { title: "Goals Conceded", field: "goals_conceded", source: "all" },
+  { title: "Penalties Saved", field: "penalties_saved", source: "all" },
+  { title: "Goals Prevented", field: "goals_prevented", source: "computed" },
+  { title: "Saves Per 90", field: "saves_per_90", source: "computed" },
+];
